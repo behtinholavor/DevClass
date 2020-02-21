aa98809aca7328ae43aa0e862ffc84d1e0633ea4
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2020-02-21 11:53:13 -0300

    first commit - PHP Burning

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"/>    
+        
+    <title>DevClass</title>
+</head>
+<body>
+    <div class="container">
+        <?php 
+            print_r($_GET);
+        
+        ?>    
+    </div>    
+</body>
+</html>
