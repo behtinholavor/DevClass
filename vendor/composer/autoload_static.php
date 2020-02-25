91b630f8166fee100d70c53fdd1da09e21778e39
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2020-02-25 11:29:29 -0300

    Installation from PHP Successully

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,9 +14,25 @@ class ComposerStaticInite4d35884b74d8451ae9844a707489d22
         '832debbbcfeab8700db6cef00613e4fc' => __DIR__ . '/../..' . '/app/functions/email.php',
     );
 
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInite4d35884b74d8451ae9844a707489d22::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInite4d35884b74d8451ae9844a707489d22::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
