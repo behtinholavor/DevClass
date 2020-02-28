59b0c7efc192175659b176cedb3fea78e726414f
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2020-02-28 13:46:59 -0300

    Email stay works after modification on php post file

--- public/pages/forms/contato.php
+++ public/pages/forms/contato.php
@@ -5,24 +5,24 @@ require "../../../bootstrap.php";
 if(isEmpty()) {   
     flash('message', 'Preencha todos os campos!');
     redirect("contato");
-}
-
-$validate = validate([
-    'name' => 's',
-    'email' => 'e',
-    'subject' => 's',
-    'message' => 's'
-]);
-
-$data = [
-    'quem' => $validate->email,
-    'para' => 'robertinhou@live.com',
-    'mensagem' => $validate->message,
-    'assunto' => $validate->subject
-];
-
-if(send($data)) {
-    flash('message', 'Email enviado com sucesso!', 'success');
-    redirect("contato");
+} else {
+    $validate = validate([
+        'name' => 's',
+        'email' => 'e',
+        'subject' => 's',
+        'message' => 's'
+    ]);
+    
+    $data = [
+        'quem' => $validate->email,
+        'para' => 'robertinhou@live.com',
+        'mensagem' => $validate->message,
+        'assunto' => $validate->subject
+    ];
+    
+    if(send($data)) {
+        flash('message', 'Email enviado com sucesso!', 'success');
+        redirect("contato");
+    }    
 }
 
